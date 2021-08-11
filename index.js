3530969f695a0a53e17762fd2ea35a06b61d0aac
Nigel nderi <15790423+Ampiria@users.noreply.github.com>
2021-08-11 01:33:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class LikeButton extends React.Component {
       'Like'
     );
   }
+  const domContainer = document.querySelector('#like_button_container');
+  ReactDOM.render(e(LikeButton), domContainer);
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
